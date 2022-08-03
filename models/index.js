165503d0a8592affb8919fe53fa1118799cd2edb
leesungkyu1 <lccab2002@gmail.com>
2022-08-03 16:49:48 +0900

    first commit

---
+++ models/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
+const Sequelize = require('sequelize');
+const User = require('./user');
+const Good = require('./good');
+const Auction = require('./auction');
+
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
+
+const sequelize = new Sequelize(
+    config.database, config.username, config.password, config,
+);
+
+db.sequelize = sequelize;
+db.User = User;
+db.Good = Good;
+db.Auction = Auction;
+
+User.init(sequelize);
+Good.init(sequelize);
+Auction.init(sequelize);
+
+User.associate(db);
+Good.associate(db);
+Auction.associate(db);
+
+module.exports = db;
